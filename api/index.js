6b9476f61bb59606511962d91da1a2a29c8f98af
Marcin Krawczyk <marckraw@icloud.com>
2020-11-12 23:27:43 +0100

    Testing execa commands

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const bodyParser = require('body-parser')
 const app = require('express')()
+const execa = require('execa')
 
 app.use(bodyParser.json())
 app.all('/getJSON', (req, res) => {
   res.json({ data: 'data' })
+})
+
+app.all('/testCmd', (req, res) => {
+
+  const something = execa.commandSync('git --version')
+
+  console.log(something)
+
+  res.json({ data: 'data', something})
 })
 
 module.exports = app
