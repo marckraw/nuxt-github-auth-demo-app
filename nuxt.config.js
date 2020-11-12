c24c8489216cc34e09c8d8b49049261cf2b1d4e5
Marcin Krawczyk <marckraw@icloud.com>
2020-11-12 12:00:31 +0100

    Add mode universal:

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,14 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  env: {
+    someEnv: process.env.SOME_ENV || 'no-env'
+  },
+  generate: {
+    fallback: true
+  },
+  mode: 'universal',
+
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
